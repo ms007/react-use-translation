7789420894b23cfcf778bba32cc8331c9213a0fa
Timo Jokinen <timojannejokinen@gmail.com>
2019-04-25 22:29:42 +0200

    fragment instead of span

--- src/translate.js
+++ src/translate.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 import useTranslation from './translation'
 
-const Translate = ({ path, ...props }) => {
-  const text = useTranslation(path, props)
-  return <span {...props}>{text}</span>
+const Translate = ({ path }) => {
+  const text = useTranslation(path)
+  return <React.Fragment>{text}</React.Fragment>
 }
 
 export default Translate
